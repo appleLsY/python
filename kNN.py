cf67a27e6a2f65c7eccab49af14480d4914156c6
lsy <liushuyubear@163.com>
2018-03-02 15:38:35 +0800

    classify person

--- kNN.py
+++ kNN.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from numpy import *
 import operator
 
@@ -31,3 +32,65 @@ def classify(inX,dataSet,labels,k):
     #sorted是内置排序函数
     sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
+
+#从文本文件中解析数据
+def file2matrix(filename):
+    fr=open(filename)
+    #文件中的总列数
+    arrayOLines=fr.readlines()
+    #len 代表是元素个数
+    numberOfLines=len(arrayOLines)
+    #zeros是返回一个double类零矩阵，其中有numberOfLines行，3列
+    returnMat=zeros((numberOfLines,3))
+    classLabelVector=[]
+    index=0
+    for line in arrayOLines:
+        #strip() 方法用于移除字符串头尾指定的字符（默认为空格）
+        line=line.strip()
+        #split()通过指定分隔符对字符串进行切片，如果参数num 有指定值，则仅分隔 num 个子字符串
+        listFromLine=line.split('\t')
+        returnMat[index,:]=listFromLine[0:3]
+        #append() 方法用于在列表末尾添加新的对象
+        classLabelVector.append(int(listFromLine[-1]))
+        index+=1
+    return returnMat,classLabelVector
+
+#归一化特征值
+def autoNorm(dataSet):
+    minVals=dataSet.min(0)
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals
+    normDataSet=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataSet=dataSet-tile(minVals,(m,1))
+    normDataSet=normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
+#测试代码
+def datingClassTest():
+    hoRatio=0.10
+    datingDataMat,datingLabels=file2matrix('datingTestSet.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat)
+    m=normMat.shape[0]
+    numTestVecs=int(m*hoRatio)
+    errorCoun=0.0
+    for i in range(numTestVecs):
+        classifierResult=classify(normMat[i,:],normMat[numTestVecs:m,:],\
+            datingLabels[numTestVecs:m],3)
+        print "the classifier came back with %d,the real answer is: %d"\
+            %(classifierResult,datingLabels[i])
+        if(classifierResult!=datingLabels[i]):
+            errorCoun+=1.0
+    print("the errorid %d, the total error rate is :%f" %(errorCoun,errorCoun/float(numTestVecs)))
+
+#预测代码
+def classifyPerson():
+    resultList=['not at all','in small doses','in large doses']
+    percentTats=float(raw_input("percentage of time spent playing video games?"))
+    ffMiles=float(raw_input("frequent flier miles earned per year?"))
+    iceCream=float(raw_input("liters of ice cream comsumede per year?"))
+    datingDataMat,datingLabels=file2matrix('datingTest.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat)
+    inArr=array([ffMiles,percentTats,iceCream])
+    classifierResult=classify((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print "you will probably like this person:",resultList[classifierResult-1]
