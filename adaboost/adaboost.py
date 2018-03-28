0df969b7a6ebb0b2b9cabac8fff5e3c07b31b1e7
lsy <liushuyubear@163.com>
2018-03-28 17:05:33 +0800

    adaboost over

--- adaboost/adaboost.py
+++ adaboost/adaboost.py
@@ -76,4 +76,58 @@ def adaBoostTrainDS(dataArr,classLabels,numIt=40):
         errorRate=aggErrors.sum()/m
         print('total error:',errorRate,'\n')
         if errorRate==0.0:break
-    return weakClassArr
+    return weakClassArr,aggClassEst
+
+
+
+def adaClassify(datToClass,classifierArr):
+    dataMatrix=mat(datToClass)
+    m=shape(dataMatrix)[0]
+    aggClassEst=mat(zeros((m,1)))
+    for i in range(len(classifierArr)):
+        classEst=stumpClassify(dataMatrix,classifierArr[i]['dim'],\
+            classifierArr[i]['thresh'],classifierArr[i]['ineq'])
+        aggClassEst+=classifierArr[i]['alpha']*classEst
+        print(aggClassEst)
+    return sign(aggClassEst)
+
+
+def loadDataSet(fileName):
+    numFeat=len(open(fileName).readline().split('\t'))
+    dataMat=[];labelMat=[]
+    fr=open(fileName)
+    for line in fr.readlines():
+        lineArr=[]
+        curLine=line.strip().split('\t')
+        for i in range(numFeat-1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
+
+
+def plotROC(predStrengths,classLabels):
+    import matplotlib.pyplot as plt
+    cur=(1.0,1.0)
+    ySum=0.0
+    numPosClas=sum(array(classLabels)==1.0)
+    yStep=1/float(numPosClas)
+    xStep=1/float(len(classLabels)-numPosClas)
+    sortedIndicies=predStrengths.argsort()
+    fig=plt.figure()
+    fig.clf()
+    ax=plt.subplot(111)
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index]==1.0:
+            delX=0;delY=yStep
+        else:
+            delX=xStep;delY=0
+            ySum+=cur[1]
+        ax.plot([cur[0],cur[0]-delX],[cur[1],cur[1]-delY],c='b')
+        cur=(cur[0]-delX,cur[1]-delY)
+    ax.plot([0,1],[0,1],'b--')
+    plt.xlabel('False Positive Rate');plt.ylabel('True Positive Rate')
+    plt.title('ROC curve for adaboost horse colic detection system')
+    ax.axis([0,1,0,1])
+    plt.show()
+    print('the area under the curve is:',ySum*xStep)
