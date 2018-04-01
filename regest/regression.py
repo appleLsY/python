c7122f483a4b6b8350b8fe06c6fbf3e6067114dd
lsy <liushuyubear@163.com>
2018-04-01 17:34:43 +0800

    ridge regression

--- regest/regression.py
+++ regest/regression.py
@@ -24,3 +24,53 @@ def standRegres(xArr,yArr):
         return
     ws=xTx.I*(xMat.T*yMat)
     return ws
+
+
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    xMat=mat(xArr);yMat=mat(yArr).T
+    m=shape(xMat)[0]
+    weights=mat(eye((m)))
+    for j in range(m):
+        diffMat=testPoint-xMat[j,:]
+        weights[j,j]=exp(diffMat*diffMat.T/(-2.0*k**2))
+    xTx=xMat.T*(weights*xMat)
+    if linalg.det(xTx)==0.0:
+        print('this matrix is singular,cannot do inverse')
+        return
+    ws=xTx.I*(xMat.T*(weights*yMat))
+    return testPoint*ws
+
+
+def lwlrTest(testArr,xArr,yArr,k=1.0):
+    m=shape(testArr)[0]
+    yHat=zeros(m)
+    for i in range(m):
+        yHat[i]=lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+
+def rssError(yArr,yHatArr):
+    return ((yArr-yHatArr)**2).sum()
+
+def ridgeRegres(xMat,yMat,lam=0.2):
+    xTx=xMat.T*xMat
+    denom=xTx+eye(shape(xMat)[1])*lam
+    if linalg.det(denom)==0.0:
+        print('this matrix is singular,cannot do inverse')
+        return 
+    ws=denom.I*(xMat.T*yMat)
+    return ws
+
+
+def ridgeTest(xArr,yArr):
+    xMat=mat(xArr);yMat=mat(yArr).T
+    yMean=mean(yMat,0)
+    yMat=yMat-yMean
+    xMeans=mean(xMat,0)
+    xVar=var(xMat,0)
+    xMat=(xMat-xMeans)/xVar
+    numTestPts=30
+    wMat=zeros((numTestPts,shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws=ridgeRegres(xMat,yMat,exp(i-10))
+        wMat[i,:]=ws.T
+    return wMat
